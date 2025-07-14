5b771fce524980811adac50f87629f1d9b3318e9
Passsoni08 <passonirafael@gmail.com>
2025-07-14 16:00:01 -0300

    JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
 
 //Consumindo API do Fork
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Passsoni08/js-developer-portfolio/main/data/profile.json';
-    const fetching = await fetch(url);
-    return await fetching.json();
+  const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
